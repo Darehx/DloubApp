2e21c8943fae4c1da079a8b8bca4de675742c510
Darehx <jd.dorante2@gmail.com>
2025-01-02 16:32:49 -0300

    ViewSet Conf

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,58 @@
 from rest_framework import serializers
-from .models import FormQuestion, FormResponse
+from .models import FormQuestion, FormResponse, Form
+
+
 
 class FormQuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = FormQuestion
         fields = '__all__'
-        
+
+
 class FormResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = FormResponse
-        fields = '__all__'
+        fields = ['customer', 'form', 'question', 'text']
+
+class FormResponseBulkCreateSerializer(serializers.Serializer):
+    customer = serializers.IntegerField()
+    form = serializers.IntegerField()
+    responses = serializers.ListField(
+        child=serializers.DictField(
+            child=serializers.CharField(max_length=1000)
+        )
+    )
+
+    def validate(self, data):
+        # Validar si el formulario existe
+        try:
+            form = Form.objects.get(pk=data['form'])
+            data['form_instance'] = form
+        except Form.DoesNotExist:
+            raise serializers.ValidationError({"form": "Formulario no encontrado."})
+
+        # Validar preguntas
+        question_ids = [response["question"] for response in data["responses"]]
+        valid_questions = FormQuestion.objects.filter(id__in=question_ids, form=data['form'])
+        if len(valid_questions) != len(question_ids):
+            raise serializers.ValidationError({"responses": "Algunas preguntas no son válidas para este formulario."})
+
+        return data
+
+    def create(self, validated_data):
+        customer_id = validated_data['customer']
+        form_instance = validated_data['form_instance']
+        responses_data = validated_data['responses']
+
+        # Crear objetos para insertar en la BD
+        responses = [
+            FormResponse(
+                customer_id=customer_id,
+                form=form_instance,
+                question_id=response['question'],
+                text=response['text']
+            )
+            for response in responses_data
+        ]
+        FormResponse.objects.bulk_create(responses)
+        return responses
--- api/views.py
+++ api/views.py
@@ -1,11 +1,21 @@
+from rest_framework.decorators import action
 from rest_framework import viewsets
 from .models import FormQuestion, FormResponse
-from .serializers import FormQuestionSerializer, FormResponseSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import FormResponseSerializer, FormResponseBulkCreateSerializer
 
-class FormQuestionViewSet(viewsets.ModelViewSet):
-    queryset = FormQuestion.objects.all()
-    serializer_class = FormQuestionSerializer
-    
 class FormResponseViewSet(viewsets.ModelViewSet):
     queryset = FormResponse.objects.all()
     serializer_class = FormResponseSerializer
+
+    @action(detail=False, methods=['post'])
+    def bulk_create(self, request):
+        """
+        Endpoint para crear múltiples respuestas en una sola solicitud.
+        """
+        serializer = FormResponseBulkCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Respuestas creadas exitosamente."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
